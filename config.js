57a5d55ba39bd0e83c756d0e753399a4adabee11
c1495616 <c1495616@gmail.com>
2016-09-17 18:18:25 +0800

    done

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.db = {};
+
+config.webhost = 'http://192.168.2.105:3000/';
+
+config.db.host = '192.168.2.105';
+config.db.name = 'url_shortener';
+
+module.exports = config;
